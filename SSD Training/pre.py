644340bd1ef9145aa99142844e25a870b8c239a7
rakhmeen <88541672+rakhmeen@users.noreply.github.com>
2024-08-30 13:28:22 +0500

    Update pre.py

--- SSD Training/pre.py
+++ SSD Training/pre.py
@@ -1,26 +1,25 @@
 import os
 import shutil
-
-# Paths to the original folders
+ 
 original_folders = {
     'train': {'images': 'train/images', 'labels': 'train/labels'},
     'test': {'images': 'test/images', 'labels': 'test/labels'},
     'val': {'images': 'val/images', 'labels': 'val/labels'}
 }
 
-# Paths to the new dataset structure
+ 
 new_structure = {
     'train': {'images': 'dataset/images/train', 'labels': 'dataset/labels/train'},
     'test': {'images': 'dataset/images/test', 'labels': 'dataset/labels/test'},
     'val': {'images': 'dataset/images/val', 'labels': 'dataset/labels/val'}
 }
 
-# Ensure the new directories exist
+ 
 for key in new_structure:
     os.makedirs(new_structure[key]['images'], exist_ok=True)
     os.makedirs(new_structure[key]['labels'], exist_ok=True)
 
-# Move the files
+ 
 for key in original_folders:
     for folder_type in ['images', 'labels']:
         src_folder = original_folders[key][folder_type]
