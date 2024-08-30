463f059a7924c5a5f505263f8f0fc5f87684ccbd
rakhmeen <88541672+rakhmeen@users.noreply.github.com>
2024-08-30 13:32:27 +0500

    Update test.py

--- Yolo Training/test.py
+++ Yolo Training/test.py
@@ -7,8 +7,7 @@ sizes = []
 for img_file in os.listdir(image_dir):
     with Image.open(os.path.join(image_dir, img_file)) as img:
         sizes.append(img.size)
-
-# Calculate average image size
+ 
 avg_width = sum(size[0] for size in sizes) / len(sizes)
 avg_height = sum(size[1] for size in sizes) / len(sizes)
 
