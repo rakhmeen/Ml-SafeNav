84f94b8a844c1c04f80958e83e55dda48f8a67e5
rakhmeen <88541672+rakhmeen@users.noreply.github.com>
2024-08-30 13:34:39 +0500

    Update rename_files.py

--- Data Preprocessing/rename_files.py
+++ Data Preprocessing/rename_files.py
@@ -1,6 +1,6 @@
 import os
 
-# Set the directory containing your images
+ 
 folder_path = 'cloudy and medium wave/text'
 
 # Iterate through all the files in the folder
@@ -9,11 +9,11 @@ for filename in os.listdir(folder_path):
         # Replace "CamFeed" with "cloudy_and_medium_wave"
         new_filename = filename.replace("RawMeta", "cloudy_and_medium_wave")
         
-        # Get the full file paths
+        
         old_file = os.path.join(folder_path, filename)
         new_file = os.path.join(folder_path, new_filename)
         
-        # Rename the file
+      
         os.rename(old_file, new_file)
 
 print("Files renamed successfully!")
