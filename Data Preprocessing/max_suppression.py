7a7f3833ee487a4b084cc2b7a448693e747a4216
rakhmeen <88541672+rakhmeen@users.noreply.github.com>
2024-08-30 13:36:29 +0500

    Update max_suppression.py

--- Data Preprocessing/max_suppression.py
+++ Data Preprocessing/max_suppression.py
@@ -2,36 +2,35 @@ import json
 import ast
 import os
 
-# Function to calculate area of a bounding box
+ 
 def calculate_area(bbox):
     width = bbox[1]["X"] - bbox[0]["X"]
     height = bbox[1]["Y"] - bbox[0]["Y"]
     return abs(width * height)
 
-# Define a threshold for the minimum area and width/height constraints
-min_area_threshold = 50  # Example threshold, adjust according to your needs
+ 
+min_area_threshold = 50  
 min_width = 10
 min_height = 30
-
-# Path to the directory containing the label files
+ 
 directory_path = 'dataset/labels/train'
 
-# Iterate through all files in the directory
+ 
 for filename in os.listdir(directory_path):
     if filename.endswith('.txt'):
         file_path = os.path.join(directory_path, filename)
         
-        # Load the JSON data from the file
+     
         with open(file_path, 'r') as file:
             json_string = file.read().strip()
 
-        # Remove outer quotes and unescape the string
+       
         json_string = ast.literal_eval(json_string)
 
-        # Parse the JSON data
+  
         data = json.loads(json_string)
 
-        # Filter out bounding boxes based on area, width, and height constraints
+    
         filtered_tracked_obj = []
         for obj in data.get("TrackedObj", []):
             bbox = obj.get("BB2D", [])
@@ -43,10 +42,9 @@ for filename in os.listdir(directory_path):
                 if bbox_area >= min_area_threshold and width >= min_width and height >= min_height:
                     filtered_tracked_obj.append(obj)
 
-        # Update the data with the filtered bounding boxes
+    
         data["TrackedObj"] = filtered_tracked_obj
-
-        # Write the filtered data back to the file
+ 
         with open(file_path, 'w') as file:
             json.dump(data, file, indent=4)
 
